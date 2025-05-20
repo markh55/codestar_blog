4716cc08d757b9effc6772a4a67ca7497613460a
Mark Hallett <markhallett5@outlook.com>
2025-05-21 00:02:45 +0100

    Set DEBUG to False and update ALLOWED_HOSTS for Heroku deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i#uo%&jgxd61df@f6+bkn_=-v&i1seblx#954!djrc-qruv3a+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.herokuapp.com']
 
 
 # Application definition
