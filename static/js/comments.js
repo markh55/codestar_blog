18bc7a389b0d6a2609ce236eabbc91e9aca6cf4a
Mark Hallett <markhallett5@outlook.com>
2025-05-31 19:37:16 +0100

    tidy code up

--- static/js/comments.js
+++ static/js/comments.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   for (let button of editButtons) {
     button.addEventListener("click", (e) => {
-      let commentId = e.target.getAttribute("comment_id");
+      let commentId = e.target.getAttribute("data-comment_id");
       let commentContent = document.getElementById(`comment${commentId}`).innerText;
       commentText.value = commentContent;
       submitButton.innerText = "Update";
@@ -23,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   for (let button of deleteButtons) {
     button.addEventListener("click", (e) => {
-      let commentId = e.target.getAttribute("comment_id");
+      let commentId = e.target.getAttribute("data-comment_id");
       deleteConfirm.href = `/${slug}/delete_comment/${commentId}/`;
       deleteModal.show();
     });
