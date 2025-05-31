1514f15de3878ab3cb6112b67f2b210819ae460f
Mark Hallett <markhallett5@outlook.com>
2025-05-31 14:55:13 +0100

    updated files to resolve heroku error

---
+++ staticfiles/js/comments.1f1d5f2a03b9.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const editButtons = document.getElementsByClassName("btn-edit");
+  const commentText = document.getElementById("id_body");
+  const commentForm = document.getElementById("commentForm");
+  const submitButton = document.getElementById("submitButton");
+
+  const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+  const deleteButtons = document.getElementsByClassName("btn-delete");
+  const deleteConfirm = document.getElementById("deleteConfirm");
+
+  // Get the slug from the hidden div
+  const slug = document.getElementById("post-data").dataset.slug;
+
+  for (let button of editButtons) {
+    button.addEventListener("click", (e) => {
+      let commentId = e.target.getAttribute("comment_id");
+      let commentContent = document.getElementById(`comment${commentId}`).innerText;
+      commentText.value = commentContent;
+      submitButton.innerText = "Update";
+      commentForm.setAttribute("action", `/${slug}/edit_comment/${commentId}/`);
+    });
+  }
+
+  for (let button of deleteButtons) {
+    button.addEventListener("click", (e) => {
+      let commentId = e.target.getAttribute("comment_id");
+      deleteConfirm.href = `/${slug}/delete_comment/${commentId}/`;
+      deleteModal.show();
+    });
+  }
+});
